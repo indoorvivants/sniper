54f1454873ba7b2c92f8d2eb42588523bd43ef73
Anton Sviridov <keynmol@gmail.com>
2025-04-20 14:06:35 +0100

    Stable deps

--- project.scala
+++ project.scala
@@ -1,17 +1,17 @@
-//> using scala 3.7.0-RC2
+//> using scala 3.7.0-RC3
 //> using platform scala-native
 //> using nativeVersion 0.5.7
 
 //> using dep com.github.lolgab::magnum::1.3.1
 //> using dep com.github.lolgab::scala-native-jdbc-sqlite::0.0.3
 //> using dep com.indoorvivants::decline-derive::0.3.1
-//> using dep tech.neander::cue4s::0.0.8+1-025c312d-SNAPSHOT
+//> using dep tech.neander::cue4s::0.0.9
 //> using dep com.indoorvivants::toml::0.3.0
 //> using dep com.outr::scribe::3.16.1
 //> using dep com.lihaoyi::os-lib::0.11.4
 //> using dep com.lihaoyi::pprint::0.9.0
 //> using dep com.lihaoyi::upickle::4.1.0
-//> using dep com.indoorvivants::trigs::0.0.2-SNAPSHOT
-//> using dep net.andimiller::decline-completion::0.1.0
+//> using dep com.indoorvivants::trigs::0.0.2
+//> using dep com.indoorvivants::decline-completion::0.1.0
 //> using resourceDir ./resources
 //> using nativeEmbedResources
